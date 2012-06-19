fe77287fb8c56f9dbf77fd9c3bf44f6b97e49edb
riku salkia <riksa@iki.fi>
2012-06-19 21:30:59 +0300

    initial Android commit

---
+++ AndroidSyncMute/src/org/riksa/syncmute/ConfigActivity.java
@@ -0,0 +1,15 @@
+package org.riksa.syncmute;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConfigActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
