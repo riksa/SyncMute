94535b2ce883e2250d375e497bd31bf2cc5e8851
riku salkia <riksa@iki.fi>
2012-06-26 14:39:26 +0300

    added parse support. implemented polling with 1s intervals for testing. TODO: use Push notifications instead.

--- AndroidSyncMute/src/org/riksa/syncmute/ConfigActivity.java
+++ AndroidSyncMute/src/org/riksa/syncmute/ConfigActivity.java
@@ -20,11 +20,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import com.parse.*;
+
+import java.util.List;
 
 public class ConfigActivity extends Activity {
 
@@ -32,6 +41,8 @@ public class ConfigActivity extends Activity {
      * Preferences instance
      */
     private SharedPreferences sharedPreferences;
+    private static final String TAG = "SyncMute_ConfigActivity";
+    private static final long POLL_DELAY = 1000;
 
     /**
      * Called when the activity is first created.
@@ -41,11 +52,94 @@ public class ConfigActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        // TODO: check if launched for the first time and show some sort of quick Getting started -dialog
+
+        Parse.initialize(this, getString(R.string.parse_application_id), getString(R.string.parse_client_key));
+
+//        ParseObject testObject = new ParseObject("SyncMuteTest");
+//        testObject.put("mute", "on");
+//        testObject.saveInBackground();
+    }
+
+    private Runnable pollRunnable;
+    private Handler handler = new Handler();
+
+    /**
+     * Polling for debug purposes, TODO: push version
+     */
+    private void debugStartPoller() {
+        final ConfigActivity configActivity = this;
+
+        if (pollRunnable == null) {
+            pollRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    ParseQuery query = new ParseQuery("ChannelState").whereEqualTo("channel", getChannel());
+                    query.setLimit(1);
+                    query.findInBackground(new FindCallback() {
+                        @Override
+                        public void done(List<ParseObject> parseObjects, ParseException e) {
+                            //To change body of implemented methods use File | Settings | File Templates.
+                            if (e == null) {
+                                for (ParseObject parseObject : parseObjects) {
+                                    Object value = parseObject.get("state");
+                                    if (value instanceof Boolean) {
+                                        Boolean state = (Boolean)value;
+                                        if( state == null ) state = false;
+                                        setChannelState(state.booleanValue());
+                                    }
+                                }
+                            } else {
+                                Log.d(TAG, "Error: " + e.getMessage());
+                            }
+                            handler.postDelayed(pollRunnable, POLL_DELAY);
+                        }
+                    });
+                }
+            };
+            handler.post(pollRunnable);
+        }
+    }
+
+    private void setChannelState(boolean stateOn) {
+        View view = findViewById(R.id.channel_state);
+        if( view instanceof ImageView ) {
+            ImageView imageView = (ImageView) view;
+
+            if( stateOn ) {
+                imageView.setImageLevel(1);
+            } else {
+                imageView.setImageLevel(0);
+            }
+
+        }
+    }
+
+    private void debugStopPoller() {
+        if (pollRunnable != null) {
+            handler.removeCallbacks( pollRunnable );
+            pollRunnable = null;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();    //To change body of overridden methods use File | Settings | File Templates.
+        debugStartPoller();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();    //To change body of overridden methods use File | Settings | File Templates.
+        debugStopPoller();
+    }
+
+    private String getChannel() {
+        return "JORMA";  //To change body of created methods use File | Settings | File Templates.
     }
 
     /**
      * Creates options menu
+     *
      * @param menu
      * @return
      */
@@ -58,6 +152,7 @@ public class ConfigActivity extends Activity {
 
     /**
      * Handle selection of a menu item
+     *
      * @param item
      * @return
      */
@@ -75,6 +170,7 @@ public class ConfigActivity extends Activity {
 
     /**
      * Handle change in configuration
+     *
      * @param newConfig
      */
     @Override
